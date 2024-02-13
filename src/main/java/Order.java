2913a417b73f80b4fe08d810304e7bdd711b6a3d
jessi <jessi.nowski@hotmail.de>
2024-02-13 10:48:19 +0100

    updated addOrder with timeStamp

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,8 +1,11 @@
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public record Order(
         String id,
         List<Product> products,
-        OrderStatus orderStatus
+        OrderStatus orderStatus,
+
+        ZonedDateTime timeStamp
 ) {
 }
