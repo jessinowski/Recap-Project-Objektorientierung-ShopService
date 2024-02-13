a6af0a9af7786f0c6e303d84b0b18ae11790a053
jessi <jessi.nowski@hotmail.de>
2024-02-13 11:04:30 +0100

    merged Main

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -4,7 +4,6 @@ import java.util.List;
 public record Order(
         String id,
         List<java.util.Optional<Product>> products,
-        OrderStatus orderStatus
         @With OrderStatus orderStatus
 ) {
 }
